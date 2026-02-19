158f894c5460045008c9c6a589a92b66d43f331f
ElishaMcC <elishamccanick@gmail.com>
2026-02-18 19:54:57 -0500

    User class skeleton

--- User.java
+++ User.java
@@ -1,1 +1,69 @@
-// this is a test change
+public class User {
+    /* Attributes for the User superclass */
+    private String name; //User's full name
+    private String username; // User's unique username for login
+    private String password; // User's password for authentication
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+/*----------------------------Setters and Getters for User superclass attributes---------------------------- */
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+/* ----------------------------User Subclasses---------------------------- */
+
+/* Subclass for station manager(s) that extends User */
+    public class stationManager extends User {
+        private int sManagerID;
+        public stationManager(String name, String username, String password) {
+            super(name, username, password);
+        }
+        /* Getter and Setter for sManagerID */
+        public int getSManagerID() {
+            return sManagerID;
+        }
+        public void setSManagerID(int sManagerID) {
+            this.sManagerID = sManagerID;
+        }
+    }
+
+
+/* Subclass for bus manager(s) that extends User */
+    public class busManager extends User {
+        private int bManagerID;
+        public busManager(String name, String username, String password) {
+            super(name, username, password);
+        }
+        /* Getter and Setter for bManagerID */
+        public int getBManagerID() {
+            return bManagerID;
+        }
+        public void setBManagerID(int bManagerID) {
+            this.bManagerID = bManagerID;
+        }
+    }
+}
