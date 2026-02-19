c508805db464313233e361e4e786aaefa87eba26
RichardsonI-E <ivane.richardson@hotmail.com>
2026-02-18 21:34:00 -0500

    Add files via upload

--- Bus.java
+++ Bus.java
@@ -1,20 +1,29 @@
 public class Bus {
     //Declaring Attributes
-    private int busID;
-    private String make;
-    private String model;
-    private float tankSize;
-    private float fuelBurnRate;
-    private float cruiseSpeed;
+    private int busID; //ID number of Bus
+    private String make; //Make of the Bus
+    private String model; //Model of Bus 
+    private float tankSize; //Size of Tank in gallons
+    private float fuelBurnRate; //Fuel burn rate in MPG
+    private float cruiseSpeed; // Most effiecent speed
 
-    //Current Methods
-        //set/get busID
+    public Bus(int busID, String make, String model, float tankSize, float fuelBurnRate, float cruiseSpeed){
+        this.busID = busID;
+        this.make = make;
+        this.model = model;
+        this.tankSize = tankSize;
+        this.fuelBurnRate = fuelBurnRate;
+        this.cruiseSpeed = cruiseSpeed;
+    }
+
+        //------------------------------Setter and getter methods-------------------//
         public void setBusID(int busID){
             this.busID = busID;
         }
         public int getBusId(){
             return busID;
         }
+
             //set/get make
         public void setMake(String make){
             this.make = make;
@@ -22,6 +31,7 @@ public class Bus {
         public String getMake(){
             return make;
         }
+
             //set/get model
         public void setModel(String model){
             this.model = model;
@@ -29,6 +39,7 @@ public class Bus {
         public String getModel(){
             return model;
         }
+
             //set/get tankSize
         public void setTankSize(float tankSize){
                 this.tankSize = tankSize;
@@ -36,6 +47,7 @@ public class Bus {
         public float getTankSize(){
                 return tankSize;
         }
+
             //set/get fuelBurnRate
         public void setFuelBurnRate(float fuelBurnRate){
             this.fuelBurnRate = fuelBurnRate;
@@ -43,6 +55,7 @@ public class Bus {
         public float getFuelBurnRate(){
             return fuelBurnRate;
         }
+
         //set/get cruiseSpeed
         public void setCruiseSpeed(float cruiseSpeed){
             this.cruiseSpeed = cruiseSpeed;
@@ -54,5 +67,7 @@ public class Bus {
 
     public static void main(String[] args) {
         System.out.println("test test");
+        Bus bus = new Bus(1234, "Travel", "Cross-Road", 80, 10, 55);
+        System.out.println(bus.getBusId() + " " + bus.getMake() + " " + bus.getModel());
     }
 }
