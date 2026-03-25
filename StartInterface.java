19b55b32a7d58d17b8f2d40fec7ba8876e1f51a4
ElishaMcC <elishamccanick@gmail.com>
2026-03-24 20:50:30 -0400

    Unknown fix

--- StartInterface.java
+++ StartInterface.java
@@ -2,10 +2,14 @@ import java.awt.CardLayout;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import screens.HomeScreen;
+import screens.LoginScreen;
 
 class StartInterface{
     public static void main(String[] args) {
