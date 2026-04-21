9f13cdf6e0d5dad6d383fe60a42b4189db15d92d
ElishaMcC <elishamccanick@gmail.com>
2026-04-21 19:41:15 -0400

    reformat on main class

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -23,82 +23,82 @@ It utilizes a CardLayout to switch between tabs/screens that are defined in the 
 exception of the disclaimer)
  */
 class StartInterface {
-    public static void main(String[] args) throws IOException {
-        new JFXPanel(); // initialize javafx to prevent later crash
+        public static void main(String[] args) throws IOException {
+                new JFXPanel(); // initialize javafx to prevent later crash
 
-        // Create a new java swing window to start the program
-        JFrame app = new JFrame("Group 6: Bus Planner");
+                // Create a new java swing window to start the program
+                JFrame app = new JFrame("Group 6: Bus Planner");
 
-        // creates layout that allows swapping between tabs/screens
-        CardLayout layout = new CardLayout();
-        // creates container that holds all screens for the program
-        JPanel container = new JPanel(layout);
+                // creates layout that allows swapping between tabs/screens
+                CardLayout layout = new CardLayout();
+                // creates container that holds all screens for the program
+                JPanel container = new JPanel(layout);
 
-        // ------------------------------------Screens-------------------------
-        JPanel startScreen = new JPanel(new GridBagLayout());
-        // creates a container to hold the startup screen
+                // ------------------------------------Screens-------------------------
+                JPanel startScreen = new JPanel(new GridBagLayout());
+                // creates a container to hold the startup screen
 
-        JLabel disclaimer = new JLabel(
-                "THIS SOFTWARE IS NOT TO BE USED FOR ROUTE PLANNING PURPOSES");
+                JLabel disclaimer = new JLabel(
+                                "THIS SOFTWARE IS NOT TO BE USED FOR ROUTE PLANNING PURPOSES");
 
-        disclaimer.setFont(new Font("Arial", Font.BOLD, 20));
+                disclaimer.setFont(new Font("Arial", Font.BOLD, 20));
 
-         // Set the JLabel to align to the center of the screen
-        disclaimer.setAlignmentX(Component.CENTER_ALIGNMENT);
+                // Set the JLabel to align to the center of the screen
+                disclaimer.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-         // create and add disclaimer to start screen for software as per instructions
-        startScreen.add(disclaimer);
+                // create and add disclaimer to start screen for software as per instructions
+                startScreen.add(disclaimer);
 
-        // refer to LoginScreen.java
-        LoginScreen loginScreen = new LoginScreen(
-                app, layout, container);
+                // refer to LoginScreen.java
+                LoginScreen loginScreen = new LoginScreen(
+                                app, layout, container);
 
-        // refer to MapScreen.java
-        MapScreen mapScreen = new MapScreen(
-                app, layout, container);
+                // refer to MapScreen.java
+                MapScreen mapScreen = new MapScreen(
+                                app, layout, container);
 
-        // refer to HomeScreen.java
-        HomeScreen homeScreen = new HomeScreen(
-                app, layout, container, mapScreen);
+                // refer to HomeScreen.java
+                HomeScreen homeScreen = new HomeScreen(
+                                app, layout, container, mapScreen);
 
-        // refer to SettingsScreen.java
-        SettingsScreen settingsScreen = new SettingsScreen(
-                app, layout, container);
+                // refer to SettingsScreen.java
+                SettingsScreen settingsScreen = new SettingsScreen(
+                                app, layout, container);
 
-        // refer to SManageScreen.java
-        SManageScreen sManageScreen = new SManageScreen(
-                app, layout, container, mapScreen);
+                // refer to SManageScreen.java
+                SManageScreen sManageScreen = new SManageScreen(
+                                app, layout, container, mapScreen);
 
-        // refer to BManageScreen.java
-        BManageScreen bManageScreen = new BManageScreen(
-                app, layout, container);
+                // refer to BManageScreen.java
+                BManageScreen bManageScreen = new BManageScreen(
+                                app, layout, container);
 
-        // refer to UManageScreen.java
-        UManageScreen uManageScreen = new UManageScreen(
-                app, layout, container);
+                // refer to UManageScreen.java
+                UManageScreen uManageScreen = new UManageScreen(
+                                app, layout, container);
 
-        // add all screens to the cardLayout
-        container.add(startScreen, "start");
-        container.add(loginScreen, "login");
-        container.add(homeScreen, "home");
-        container.add(mapScreen, "map");
-        container.add(settingsScreen, "settings");
-        container.add(sManageScreen, "sManage");
-        container.add(bManageScreen, "bManage");
-        container.add(uManageScreen, "uManage");
+                // add all screens to the cardLayout
+                container.add(startScreen, "start");
+                container.add(loginScreen, "login");
+                container.add(homeScreen, "home");
+                container.add(mapScreen, "map");
+                container.add(settingsScreen, "settings");
+                container.add(sManageScreen, "sManage");
+                container.add(bManageScreen, "bManage");
+                container.add(uManageScreen, "uManage");
 
-        app.add(container); // add the container to the JFrame
+                app.add(container); // add the container to the JFrame
 
-        // set the window size, appearance and exit on close
-        app.setSize(800, 600);
-        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.setVisible(true);
+                // set the window size, appearance and exit on close
+                app.setSize(800, 600);
+                app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                app.setVisible(true);
 
-        // Timer switches to the login screen after 3 seconds
-        Timer start = new Timer(3000, e -> {
-            layout.show(container, "login");
-        });
-        start.setRepeats(false);
-        start.start();
-    }
+                // Timer switches to the login screen after 3 seconds
+                Timer start = new Timer(3000, e -> {
+                        layout.show(container, "login");
+                });
+                start.setRepeats(false);
+                start.start();
+        }
 }
